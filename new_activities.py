f3b8b975333a173b227baa70cfd5de34fd9805af
ZoIQ01 <rusya.petrov47@gmail.com>
2025-09-09 19:56:41 +0300

    load_activities to panda

--- new_activities.py
+++ new_activities.py
@@ -1,33 +1,31 @@
 import pandas as pd
 import os
-import csv
+from const import FILENAME
 
-FILENAME = "data/activities.csv"
-FIELDNAMES = ["id", "activity", "type", "participants", "price", "accessibility", "link"]
 
-def load_activities():
-    FILENAME = "data/activities.csv"
+
+def load_activities() -> pd.DataFrame:
     os.makedirs("data", exist_ok=True)
-    activities = []
-    if not os.path.exists(FILENAME) or os.stat(FILENAME).st_size == 0:
-        print("Data haven`t been found. File will be created after save.")
-        return activities
-    with open(FILENAME, newline="", encoding="utf-8") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            row["participants"] = int(row["participants"])
-            try:
-                row["price"] = float(row["price"])
-            except:
-                row["price"] = row["price"]
-            try:
-                row["accessibility"] = float(row["accessibility"])
-            except:
-                row["accessibility"] = row["accessibility"]
-            activities.append(row)
-    return activities
 
-def add_activities(new_activities, current_activities):
+    if not os.path.exists(FILENAME) or os.stat(FILENAME).st_size == 0:
+        print("Data haven't been found. File will be created after save.")
+        columns = ["id", "activity", "type", "participants", "price", "accessibility", "link"]
+        return pd.DataFrame(columns=columns)
+
+    df = pd.read_csv(FILENAME)
+
+    if "participants" in df.columns:
+        df["participants"] = pd.to_numeric(df["participants"], errors="coerce").fillna(0).astype(int)
+
+    if "price" in df.columns:
+        df["price"] = pd.to_numeric(df["price"], errors="coerce")
+
+    if "accessibility" in df.columns:
+        df["accessibility"] = pd.to_numeric(df["accessibility"], errors="coerce")
+    return df
+
+
+def add_activities(new_activities:list[dict], current_activities:list[dict]):
     df_current = pd.DataFrame(current_activities)
     df_new = pd.DataFrame(new_activities)
     combined = pd.concat([df_current, df_new]).drop_duplicates(subset="id", ignore_index=True)
