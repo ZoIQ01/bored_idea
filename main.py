a2962fdb847ecf9e56ccf5153650035af2a2bbd7
ZoIQ01 <rusya.petrov47@gmail.com>
2025-09-09 19:54:30 +0300

    Update menu_print etc.

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from new_activities import load_activities, add_activities
 from check_activities import check_activities
 from rdm_idea import random_idea
-from filter import filter_activities
-from sort_by import sort_activities
+from sort_idea_panda import panda_sorted
+from filter import panda_filter
 from api import fetch_activity
 import os
+from const import PATH
+import pandas as pd
 
-def load_or_fetch_initial(n=100):
+
+def print_menu():
+        print("\nMain Menu")
+        print("1. Add an idea")
+        print("2. Check all ideas")
+        print("3. Filter ideas")
+        print("4. Sort by")
+        print("5. Get random idea")
+        print("0. Exit")
+
+def load_or_fetch_initial(n: int = 100):
     activities = []
 
     os.makedirs("data", exist_ok=True)
-    path = "data/activities.csv"
-    if os.path.exists(path):
+    if os.path.exists(PATH):
         activities = load_activities()
 
     if len(activities) < n:
-        print("[INFO] Checking local data..")
-        print("[WARN] Data not enough or missing. Fetching from API..")
+        print("Checking local data..")
+        print("Data not enough or missing. Fetching from API..")
 
         all_ids = {a["id"] for a in activities}
         new_ideas = []
@@ -28,7 +39,7 @@ def load_or_fetch_initial(n=100):
             if idea and idea["id"] not in all_ids and idea["id"] not in {i["id"] for i in new_ideas}:
                 new_ideas.append(idea)
             if attempts > n * 5:
-                print("[WARN] Can't get enough ideas!")
+                print("Can't get enough ideas!")
                 break
 
         activities = add_activities(new_ideas, activities)
@@ -36,7 +47,7 @@ def load_or_fetch_initial(n=100):
     print(f"Total activities loaded: {len(activities)}")
     return activities
 
-def add_ideas_from_api(current_activities):
+def add_ideas_from_api(current_activities: list) -> None:
     try:
         n = int(input("How many ideas to fetch from API? "))
         if n <= 0:
@@ -55,31 +66,25 @@ def add_ideas_from_api(current_activities):
         if idea and idea["id"] not in all_ids and idea["id"] not in {i["id"] for i in new_ideas}:
             new_ideas.append(idea)
         if attempts > n * 5:
-            print("[WARN] Can't get enough unique ideas!")
+            print("Can't get enough unique ideas!")
             break
 
     added = add_activities(new_ideas, current_activities)
     print(f"{len(new_ideas)} ideas fetched and added.")
 
 def main():
-    activities_list = load_or_fetch_initial(n=100)
+    activities_list = pd.DataFrame(load_or_fetch_initial(n=100))
     while True:
-        print("\nMain Menu")
-        print("1. Add an idea")
-        print("2. Check all ideas")
-        print("3. Filter ideas")
-        print("4. Sort by")
-        print("5. Get random idea")
-        print("0. Exit")
+        print_menu()
         choice = input("Choose an option: ")
         if choice == "1":
             add_ideas_from_api(activities_list)
         elif choice == "2":
             check_activities(activities_list)
         elif choice == "3":
-            filter_activities(activities_list)
+            activities_list = panda_filter(activities_list)
         elif choice == "4":
-            sort_activities(activities_list)
+            activities_list = panda_sorted(activities_list)
         elif choice == "5":
             random_idea(activities_list)
         elif choice == "0":
