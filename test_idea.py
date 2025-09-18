e3f7e9ba205ed3b014e3ed7e5e4025f6a9604ee1
ZoIQ01 <rusya.petrov47@gmail.com>
2025-09-18 20:04:28 +0300

    Pytest for filter and sort

--- test_idea.py
+++ test_idea.py
@@ -2,20 +2,37 @@ import pytest
 from unittest.mock import patch
 import api
 import pandas as pd
-from new_activities import add_activities
-
-
+from filter import panda_filter
+from sort_idea_panda import  sort_activities
 
 @pytest.fixture
 def sample_activities():
-    return [
-        {"id": "1", "activity": "Read", "type": "fun", "participants": 1, "price": 0.2, "accessibility": 0.5,
-         "link": ""},
-        {"id": "2", "activity": "Walk", "type": "sport", "participants": 2, "price": 0.0, "accessibility": 0.1,
-         "link": "http://link"},
-        {"id": "3", "activity": "Code", "type": "work", "participants": 1, "price": 0.0, "accessibility": 0.2,
-         "link": ""}
-    ]
+    return pd.DataFrame({
+        "id": [1, 2, 3],
+        "activity": ["watch movie", "read the book", "paint the wall"],
+        "type": ["fun", "learn", "draw"],
+        "participants": [1, 2, 3],
+        "price": [0, 0.5, 1],
+        "accessibility": ["nan", "nan", "nan"],
+        "link": ["https:", "https:", "https:"]
+    })
+
+def test_filter(sample_activities):
+    result = panda_filter(sample_activities)
+    ids = [item["id"] for item in result]
+    assert all(i >= 1 for i in ids)
+    assert len(result) == 3
+    assert set(ids) == {1, 2, 3}
+
+def test_sort_by_id_ascending(sample_activities):
+    result = sort_activities(sample_activities, column="id", ascending=True)
+    ids = [item["id"] for item in result]
+    assert ids == [1, 2, 3]
+
+def test_sort_by_price_descending(sample_activities):
+    result = sort_activities(sample_activities, column="price", ascending=False)
+    prices = [item["price"] for item in result]
+    assert prices == [1.0, 0.5, 0.0]
 
 
 def test_fetch_activity_mock():
